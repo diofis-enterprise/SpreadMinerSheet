6828cd23bf232e6a86d32b4fbed2ca69ec546cab
Diofis Enterprise <151141842+diofis-enterprise@users.noreply.github.com>
2023-11-24 16:33:41 +0800

    Add files via upload

---
+++ minersheet-odbc-connection.cmd
@@ -0,0 +1,2 @@
+odbcconf /a {CONFIGSYSDSN "MySQL ODBC 8.0 Unicode Driver" "DSN=SpreadMinerSheet|Description=Connection to SpreadMinerSheet|SERVER=sql.freedb.tech|DATABASE=freedb_minersheet|USER=freedb_minersheet|PASSWORD=9nub5MN&Yg5Dynn|PORT=3306"}
+
